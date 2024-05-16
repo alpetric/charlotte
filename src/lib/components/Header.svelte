15ea0d62e3b97a1cdd5827f1b0cbd1436d53cc6c
Alexander Petric <alex@inpher.io>
2024-05-15 23:50:40 -0400

    todo

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,5 +1,5 @@
 <div>
-	<div class="flex justify-center  bg-black h-screen">
+	<div class="flex justify-center bg-black h-screen">
 		<p class="[writing-mode:vertical-lr] px-10 text-6xl text-white font-extrabold pt-10">
 			Charlotte Wagner
 		</p>
