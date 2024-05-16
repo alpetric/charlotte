d17ecf4ce20c1e51b87178afd1df7f71282844ea
Alexander Petric <alex@inpher.io>
2024-05-16 10:38:50 -0400

    add base path

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -1,11 +1,14 @@
 <script>
-    import { base } from '$app/paths';
+	import { base } from '$app/paths';
 </script>
+
 <div>
 	<div class="flex w-full h-full justify-center gap-6">
 		<a href="{base}/" class="[writing-mode:vertical-lr] pt-10 text-5xl font-extrabold">Home</a>
-		<a href="{base}/about" class="[writing-mode:vertical-lr] pt-10 text-5xl font-extrabold">About</a>
-		<a href="{base}/ceramics" class="[writing-mode:vertical-lr] pt-10 text-5xl font-extrabold">Ceramics</a
+		<a href="{base}/about" class="[writing-mode:vertical-lr] pt-10 text-5xl font-extrabold">About</a
+		>
+		<a href="{base}/ceramics" class="[writing-mode:vertical-lr] pt-10 text-5xl font-extrabold"
+			>Ceramics</a
 		>
 		<a
 			href="mailto:ccwagner88@gmail.com"
